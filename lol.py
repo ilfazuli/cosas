e1e55ce89c1099b818a02545bbbffc8e7ab6799a
ilfazuli <101884861+ilfazuli@users.noreply.github.com>
2022-05-09 12:45:07 -0400

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,15 @@
+cont = 1
+acum = 1
+print("hola mundo!")
+num1 = int(input("Ingrese un numero ra hacer el factorial: "))
+while cont <= num1:
+    acum *= cont
+    cont += 1
+
+print(f"El factorial del numero {num1} ingresado es de {acum} ")
+
+
+
+
+
+    
